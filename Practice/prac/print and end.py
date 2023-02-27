e72560512d48d9b3428699fb4c38dfc775ebd355
Sandesh-Hase <sandeshhase15@gmail.com>
2023-02-27 20:38:29 +0530

    added printing patterns

---
+++ Practice/prac/print and end.py
@@ -0,0 +1,10 @@
+#
+# for row in range(1,6):
+#     for col in range(row):
+#         print("*",end=" ")
+#     print(end="\n")
+
+for row in range(1,6):
+    for col in range(row):
+        print("*",end=" ")
+    print()
